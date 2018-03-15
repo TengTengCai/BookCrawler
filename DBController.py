890c7969ee3d2dbfa8244dc19c8e41f1abc3d3cd
Administrator <1021766585@qq.com>
2018-03-15 22:41:23 +0800

    2018-01-28 16:41:04 解码异常抛出 2018-01-28 20:48:22 出版社，出版时间没有的情况下，BUG修复 2018-01-28 21:42:56 出现空内容时，删除对应的空 2018-02-02 12:03:22 数据库连接异常处理 2018-03-06 09:59:53 test文件改名为main,mian函数采用更加优雅的写法 2018-03-13 08:45:55 数据库连接添加用户名和密码

--- DBController.py
+++ DBController.py
@@ -19,11 +19,11 @@ class Controller(object):
 
     # 初始化构造方法，对数据库的地址数据库名称进行初始化
     def __init__(self):
-        self.conn = MongoClient("123.207.85.99:27017",
-                                username='book',
-                                password='tianjun223.',
-                                authSource='book',
-                                authMechanism='SCRAM-SHA-1')
+        self.conn = MongoClient("123.207.85.99:27017",  # 服务器地址和端口
+                                username='book',  # 用户名
+                                password='tianjun223.',  # 密码
+                                authSource='book',  # 数据库名
+                                authMechanism='SCRAM-SHA-1')  # 用户验证方式
         self.db = self.conn.get_database("book")
         self.books = self.db.book
         self.urls = self.db.urls
